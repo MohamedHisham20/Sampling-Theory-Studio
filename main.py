702609530eed8432a8f90bf3ca31e50c5911ffda
Ibrahim-Fateen <80049894+Ibrahim-Fateen@users.noreply.github.com>
2024-10-29 01:17:19 +0300

    still connecting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel,
-                               QListWidget, QSlider, QLineEdit, QListWidgetItem, QCheckBox, QComboBox)
+                               QListWidget, QSlider, QLineEdit, QListWidgetItem, QCheckBox, QComboBox, QDoubleSpinBox)
 from PySide6.QtCore import QFile
 from pyqtgraph.Qt import QtCore
 
@@ -9,7 +9,6 @@ from SignalClasses import Signal
 from TimeDomainGraphs import TimeDomainGraphs
 from SignalReconstruction import SignalReconstruction
 from DFTGraph import DFTGraph
-
 
 
 class SamplingStudio(QMainWindow):
@@ -33,7 +32,7 @@ class SamplingStudio(QMainWindow):
         # controls_layout = QHBoxLayout()
 
         # Frequency Slider
-        # self.freq_slider = QSlider(QtCore.Qt.Horizontal)
+        self.freq_slider = self.ui.findChild(QSlider, "compose_freq_slider")
         # self.freq_slider.setMinimum(1)
         # self.freq_slider.setMaximum(80)
         # self.freq_slider.setValue(30)
@@ -41,7 +40,7 @@ class SamplingStudio(QMainWindow):
         # controls_layout.addWidget(self.freq_slider)
 
         # Phase slider
-        # self.phase_slider = QSlider(QtCore.Qt.Horizontal)
+        self.phase_slider = self.ui.findChild(QSlider, "compose_phase_slider")
         # self.phase_slider.setMinimum(0)
         # self.phase_slider.setMaximum(360)
         # self.phase_slider.setValue(0)
@@ -49,30 +48,39 @@ class SamplingStudio(QMainWindow):
         # controls_layout.addWidget(self.phase_slider)
 
         # Amplitude input
-        # self.amplitude_input = QLineEdit()
-        # self.amplitude_input.setText("1.0")
+        self.amplitude_input = self.ui.findChild(QDoubleSpinBox, "compose_amplitude_spin_box")
+        self.amplitude_input.setValue(1.0)
         # controls_layout.addWidget(QLabel("Amplitude:"))
         # controls_layout.addWidget(self.amplitude_input)
 
         # SNR slider
-        # self.snr_slider = QSlider(QtCore.Qt.Horizontal)
+        self.snr_slider = self.ui.findChild(QSlider, "snr_slider")
+        self.snr_slider.setValue(Signal.MAXIMUM_SNR)
+
         # self.snr_slider.setMinimum(1)
         # self.snr_slider.setMaximum(Signal.MAXIMUM_SNR)
-        # self.snr_slider.setValue(Signal.MAXIMUM_SNR)
         # controls_layout.addWidget(QLabel("SNR:"))
         # controls_layout.addWidget(self.snr_slider)
 
         # Noise checkbox
-        # self.noise_checkbox = QCheckBox("Show Noise")
-        # self.noise_checkbox.setChecked(True)
+        self.noise_checkbox = self.ui.findChild(QCheckBox, "show_noise_checkBox")
+        self.noise_checkbox.setChecked(True)
         # controls_layout.addWidget(self.noise_checkbox)
 
         # Checkbox to show samples
-        # self.show_samples_checkbox = QCheckBox("Show Samples")
-        # self.show_samples_checkbox.setChecked(True)
+        self.show_samples_checkbox = self.ui.findChild(QCheckBox, "show_samples_checkBox")
+        self.show_samples_checkbox.setChecked(True)
         # controls_layout.addWidget(self.show_samples_checkbox)
 
         # Sampling frequency slider
+        self.sampling_freq_combobox = self.ui.findChild(QComboBox, "sampling_freq_comboBox")
+        self.sampling_freq_combobox.addItem("0.25", 0.25)
+        self.sampling_freq_combobox.addItem("0.5", 0.5)
+        self.sampling_freq_combobox.addItem("1", 1)
+        self.sampling_freq_combobox.addItem("2", 2)
+        self.sampling_freq_combobox.addItem("5", 5)
+        self.sampling_freq_combobox.addItem("10", 10)
+        self.sampling_freq_combobox.setCurrentIndex(3)
         # self.sampling_freq_slider = QSlider(QtCore.Qt.Horizontal)
         # self.sampling_freq_slider.setMinimum(1)
         # self.sampling_freq_slider.setMaximum(10)
@@ -81,22 +89,22 @@ class SamplingStudio(QMainWindow):
         # controls_layout.addWidget(self.sampling_freq_slider)
 
         # Combo box for reconstruction techniques
-        self.reconstruction_method_combobox = self.ui.findChild(QComboBox, "reconstruction_method_combobox")
+        self.reconstruction_method_combobox = self.ui.findChild(QComboBox, "reconstruction_method_comboBox")
         self.reconstruction_method_combobox.addItem("Zero Order Hold", SignalReconstruction.ZERO_ORDER_HOLD)
         self.reconstruction_method_combobox.addItem("Linear", SignalReconstruction.LINEAR)
         self.reconstruction_method_combobox.addItem("Nyquist", SignalReconstruction.NYQUIST)
         self.reconstruction_method_combobox.addItem("Cubic Spline", SignalReconstruction.CUBIC_SPLINE)
         self.reconstruction_method_combobox.addItem("Fourier", SignalReconstruction.FOURIER)
         self.reconstruction_method_combobox.addItem("Nearest Neighbor", SignalReconstruction.NEAREST_NEIGHBOR)
-        controls_layout.addWidget(QLabel("Reconstruction Technique:"))
-        controls_layout.addWidget(self.reconstruction_method_combobox)
+        # controls_layout.addWidget(QLabel("Reconstruction Technique:"))
+        # controls_layout.addWidget(self.reconstruction_method_combobox)
 
         # Button to add component
-        self.add_button = QPushButton("Add Component")
-        controls_layout.addWidget(self.add_button)
+        self.add_button = self.ui.findChild(QPushButton, "add_component_button")
+        # controls_layout.addWidget(self.add_button)
 
         # List to display components
-        self.components_list = QListWidget()
+        self.components_list = self.ui.findChild(QListWidget, "components_listWidget")
         # layout.addLayout(controls_layout)
         # layout.addWidget(self.components_list)
 
@@ -105,13 +113,25 @@ class SamplingStudio(QMainWindow):
 
         self.DFTGraph = DFTGraph()
 
-        graph_layout = QVBoxLayout()
-        graph_layout.addWidget(self.time_domain_graphs.signal_plot)
-        graph_layout.addWidget(self.time_domain_graphs.reconstruction_plot)
-        graph_layout.addWidget(self.time_domain_graphs.difference_plot)
-        graph_layout.addWidget(self.DFTGraph.DFT_plot_widget)
-
+        self.ui.findChild(QWidget, "top_left_widget").layout().addWidget(self.time_domain_graphs.signal_plot)
+        self.ui.findChild(QWidget, "top_right_widget").layout().addWidget(self.time_domain_graphs.reconstruction_plot)
+        self.ui.findChild(QWidget, "bottom_left_widget").layout().addWidget(self.time_domain_graphs.difference_plot)
+        self.ui.findChild(QWidget, "bottom_right_widget").layout().addWidget(self.DFTGraph.DFT_plot_widget)
         # layout.addLayout(graph_layout)
+
+        top_right_label = self.ui.findChild(QLabel, "top_right_label")
+        top_left_label = self.ui.findChild(QLabel, "top_left_label")
+        bottom_left_label = self.ui.findChild(QLabel, "bottom_left_label")
+        bottom_right_label = self.ui.findChild(QLabel, "bottom_right_label")
+
+        if top_right_label:
+            top_right_label.setParent(None)
+        if top_left_label:
+            top_left_label.setParent(None)
+        if bottom_left_label:
+            bottom_left_label.setParent(None)
+        if bottom_right_label:
+            bottom_right_label.setParent(None)
 
         # Connect signals and slots
         self.add_button.clicked.connect(self.add_component)
@@ -120,7 +140,7 @@ class SamplingStudio(QMainWindow):
         self.amplitude_input.textChanged.connect(self.update_active_component)
         self.snr_slider.valueChanged.connect(self.update_snr)
         self.noise_checkbox.stateChanged.connect(self.plot_signal)
-        self.sampling_freq_slider.valueChanged.connect(self.plot_signal)
+        self.sampling_freq_combobox.currentIndexChanged.connect(self.plot_signal)
         self.reconstruction_method_combobox.currentIndexChanged.connect(self.plot_signal)
         self.components_list.itemDoubleClicked.connect(self.remove_component)
 
@@ -140,7 +160,7 @@ class SamplingStudio(QMainWindow):
         self.signal.add_frequency_component()
         self.update_component_list()
         self.plot_signal()
-        self.amplitude_input.setText("0")
+        self.amplitude_input.setValue(0)
         self.update_active_component()
 
     def remove_component(self, item):
@@ -158,7 +178,7 @@ class SamplingStudio(QMainWindow):
             self.components_list.addItem(list_item)
 
     def update_snr(self):
-        self.signal.SNR = self.snr_slider.value()
+        self.signal.SNR = self.get_snr()
         self.plot_signal()
 
     def plot_signal(self):
@@ -169,7 +189,7 @@ class SamplingStudio(QMainWindow):
         self.time_domain_graphs.draw_signal(self.signal.linspace, data_points)
 
         # Plot sampled data points if needed
-        sampled_data, sample_linspace = self.signal.get_samples(self.sampling_freq_slider.value(), with_noise)
+        sampled_data, sample_linspace = self.signal.get_samples(self.sampling_freq_combobox.currentData(), with_noise)
         if self.show_samples_checkbox.isChecked():
             self.time_domain_graphs.draw_samples(sample_linspace, sampled_data)
 
@@ -178,7 +198,7 @@ class SamplingStudio(QMainWindow):
 
         # Perform reconstruction with the selected technique
         maximum_frequency = self.signal.get_maximum_frequency()
-        sampling_frequency = self.sampling_freq_slider.value() * maximum_frequency
+        sampling_frequency = self.sampling_freq_combobox.currentData() * maximum_frequency
         reconstruction_obj = SignalReconstruction(sampled_data, sampling_frequency, self.signal.linspace)
         reconstruction_data = reconstruction_obj.reconstruct_signal(selected_technique)
 
@@ -191,6 +211,13 @@ class SamplingStudio(QMainWindow):
         #DFT Magnitude Plot
         og_sampling_frequency = 1 / (self.signal.linspace[1]-self.signal.linspace[0])
         self.DFTGraph.draw_DFT_magnitude(data_pnts=data_points, sampling_frequency=og_sampling_frequency)
+
+    def get_snr(self):
+        value = self.snr_slider.value()
+        if value <= 10:
+            return value
+        value = value - 9
+        return 10 ** value
 
 
 if __name__ == "__main__":
